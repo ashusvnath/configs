c7e0adbede100371bd25f1f11a60d8b890e4c636
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2015-08-15 00:56:06 +0530

    Adding script to use an existing ssh-agent or start a new one and add default identities

---
+++ shell/start-ssh-agent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ -f "${HOME}/.ssh-agent-details" ] && source "${HOME}/.ssh-agent-details" > /dev/null
+
+[ `pidof ssh-agent | grep ${SSH_AGENT_PID} | wc -l` -eq 0 ] && ssh-agent > "${HOME}/.ssh-agent-details"
+
+source "${HOME}/.ssh-agent-details" > /dev/null
+
+[ `ssh-add -L | grep "No identities" | wc -l` -eq 1 ] && ssh-add
